9447fa0c068367a56ca444fb54716db7a7594466
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-06-22 19:09:06 +0300

    add main structure of the project

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Lead from '../components/Lead';
+import Main from '../components/Main';
+
+function App() {
+  return (
+    <div className="page">
+      < Header />
+      < Lead />
+      < Main />
+    </div>
+  );
+}
+
+export default App;
