522d8294eeb1841caa9993948c6492aa5fee05a9
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-06-23 12:11:45 +0300

    fix: add main section to Lead

--- src/components/Lead.js
+++ src/components/Lead.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function Lead() {
   return (
-    <section className='lead'>
-      <h1 className='lead__title'>Portfolio</h1>
-      <p className='lead__paragraph'>Agency provides a full service range including technical skills, design, business understanding.</p>
-    </section>
+    <main className='content'>
+      <section className='lead'>
+        <h1 className='lead__title'>Portfolio</h1>
+        <p className='lead__paragraph'>Agency provides a full service range including technical skills, design, business understanding.</p>
+      </section>
+    </main>
   );
 }
 
