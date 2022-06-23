c4a3d196dc06d3fbafce8914d4c2a5816038083e
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-06-23 12:06:29 +0300

    feat: handle lead layout for mobile

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Lead from '../components/Lead';
 
 function App() {
   return (
     <div className="page">
       < Header />
+      < Lead />
     </div>
   );
 }
---
+++ src/components/Lead.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Lead() {
+  return (
+    <section className='lead'>
+      <h1 className='lead__title'>Portfolio</h1>
+      <p className='lead__paragraph'>Agency provides a full service range including technical skills, design, business understanding.</p>
+    </section>
+  );
+}
+
+export default Lead;
