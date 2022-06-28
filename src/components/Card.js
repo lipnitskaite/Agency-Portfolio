eef3801cf5271ec2ca0e7428b7b895ebf8a1a30d
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-06-28 21:03:58 +0300

    feat: save pictures for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Sofa1 from '../images/Sofa-Background.svg';
 
 function Card() {
-    return (
-      <article className='card page__card'>
-        <button className='card__button'>Design</button>
-        <h2 className='card__title'>Sofa</h2>
-      </article>
-    );
+  const cards = [
+    {
+      name: 'Sofa',
+      tag: 'Design',
+      imageSrc: Sofa1,
+      imageAlt: 'Background decorative image'
+    }
+  ];
+  
+
+  return (
+    <article className='card page__card'>
+      <img src={cards.imageSrc} alt={cards.imageAlt}></img>
+      <button className='card__button'>Design</button>
+      <h2 className='card__title'>Sofa</h2>
+    </article>
+  );
 }
 
 export default Card;
---
+++ src/utils/data.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sofa1 from '../images/Sofa-Background.svg';
+
+const cards = [
+  {
+    name: 'Sofa',
+    tag: 'Design',
+    imageSrc: Sofa1,
+    imageAlt: 'Background decorative image'
+  }
+];
+
+export default cards;
